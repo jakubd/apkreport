383055641961a199faa892d93f46f9ea83318ec1
jakub <jakub.dalek@utoronto.ca>
2021-08-02 13:39:36 -0400

    apkreport struct added

--- apkreportlib/mobsf_api_structs.go
+++ apkreportlib/mobsf_api_structs.go
@@ -24,3 +24,22 @@ type MobSFRecentScansResults struct {
 	numPages int
 	results []MobSFAppIndex
 }
+
+type APKReport struct {
+	packageName string 				// package_name
+	filename string					// file_name
+	md5 string						// md5
+	sha1 string						// sha1
+	sha256 string					// sha256
+	size string						// size
+	playTitle string				// playstore_details.title
+	playDesc string					// playstore_details.description
+	playInstall string				// playstore_details.installs
+	playDeveloper string			// playstore_details.developer
+	playDevWebsite string			// playstore_details.developerWebsite
+	playDevAddress string			// playstore_details.developerAddress
+	playUrl string					// playstore_details.url
+	playVersion string				// playstore_details.version
+	playPrivacyUrl string			// playstore_details.privacyPolicy
+
+}
