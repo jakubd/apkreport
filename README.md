97576032d0a8559900d590fb087dfc47bea155c4
jakub <jakub.dalek@utoronto.ca>
2021-08-03 10:02:57 -0400

    lets make a quick main for now

--- README.md
+++ README.md
@@ -5,28 +5,31 @@
 Generate a csv report of basic app information from the 
 [MobSF](https://github.com/MobSF/Mobile-Security-Framework-MobSF) API.
 
-
-
-Sample input:
-
-```text
-
-```
-
-Then run
+Sample run
 
 ```bash
+./apkreport
 ```
 
-Sample output:
+You will have to edit the generated config file (with your API info): `~/.apkreport.yml`
+and run again.
+
+Sample output columns:
 
 ```csv
-
+package_name, filename, md5, play_prog_title, play_version, play_url, size, play_installs, play_dev	play_dev_site, play_dev_address
 ```
 
 ## Install
 
-In order to install simply
+In order to install simply cd to this directory and do:
 
 ```bash
+make
+```
+
+If you want to move the binary to `/usr/bin/local`:
+
+```bash
+make install
 ```
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jakubd/apkreport/apkreportlib"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func setUpLogger() {
@@ -13,8 +16,47 @@ func setUpLogger() {
 }
 
 func main() {
-
 	setUpLogger()
-	log.Info("started main func")
-	log.Info("ended main func")
+	log.Info("running apkreport...")
+
+	defaultCfg, err := apkreportlib.GetDefaultConfigFn()
+	if err != nil {
+		log.Error(err)
+		os.Exit(255)
+	}
+
+	if _, err := os.Stat(defaultCfg); os.IsNotExist(err) {
+		log.Info("API config file doesn't exist creating .apkreport.yml in: " + defaultCfg )
+		errCfgCreate := apkreportlib.CreateDefaultConfig()
+		if errCfgCreate != nil {
+			log.Error("could not create config at" + defaultCfg)
+			os.Exit(255)
+		}
+		fmt.Println("please edit config file with your API info ", defaultCfg, "and rerun")
+		os.Exit(0)
+	}
+
+	outFn := "apkreport-out.csv"
+	apiInfo, apiErr := apkreportlib.GetApiInfoFromConfig()
+
+	if apiErr != nil {
+		log.Error("Error parsing config file: ", apiErr)
+		os.Exit(255)
+	}
+	log.Info("found config file at " + defaultCfg)
+	log.Info("running basic report of all scans to file " + outFn)
+
+	allRes, repErr := apkreportlib.BasicReport(apiInfo)
+	if repErr != nil {
+		log.Error("Error running report", repErr)
+		os.Exit(255)
+	}
+
+	errCsv := apkreportlib.ApkReportSliceToCsv(allRes, outFn)
+	if errCsv != nil {
+		log.Error("error writing output file ", errCsv)
+		os.Exit(255)
+	}
+
+	log.Info("done")
 }
